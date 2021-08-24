d4fea003bb1a2a74a528001c370e7f253b64e706
climb719 <mary.oldweiler@gmail.com>
2021-08-24 13:02:57 -0400

    commit tree 351a362db67671304aaa0c6e42a0de9d0c069057

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9\s'-]/g, "")
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+     const words =  string.split(' ')
+     const intersection = exceptions.filter(element => words.includes(element));
+  //  if (words === words[0] || !intersection) {
+       const newWords = words.map(word =>
+        word[0].toUpperCase() + word.substring(1))
+        const newString = newWords.join(" ")
+        return newString
+     //}
+    }
+
+  }
